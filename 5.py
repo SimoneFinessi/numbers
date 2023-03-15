dd26390668d18b756786febee0274a3102fbb949
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-03-15 13:01:22 +0000

    finito es 5

--- 5.py
+++ 5.py
@@ -1,19 +1,22 @@
 from flask import Flask,render_template
 import random
 app = Flask(__name__)
-
+x=0
 @app.route('/', methods=['GET'])
 def tour():
-    return render_template("numeri4.html")
+    return render_template("numeri5.html")
 
 @app.route('/numero/<num>', methods=['GET'])
 def indovina(num):
-  
+  global x
+  x+=1
   ran=random.randint(1,9)
+  print(ran)
   if int(num)==ran:
-    return render_template("vinto4.html")
+    vinto=x
+    x=0
+    return render_template("vinto5.html",tenta=vinto)
   else:
-    return render_template("perso4.html")
-
+    return render_template("perso5.html",tenta=x)
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
