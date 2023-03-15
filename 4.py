cc56f66ac43d564889e6297b46cf71b97912b91f
SimoneFinessi <finessi.simone@iisgalvanimi.edu.it>
2023-03-15 12:52:19 +0000

    finito es 4

---
+++ 4.py
@@ -0,0 +1,20 @@
+from flask import Flask,render_template
+import random
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def tour():
+    return render_template("numeri4.html")
+
+@app.route('/numero/<num>', methods=['GET'])
+def indovina(num):
+  ran=random.randint(1,9)
+  if int(num)==ran:
+    return render_template("vinto4.html")
+  else:
+    return render_template("perso4.html")
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
